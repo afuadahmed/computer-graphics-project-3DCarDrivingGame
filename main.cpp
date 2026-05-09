f46cd648ca600430383e43920c319d9185e185e5
Fuad Ahmed <afuadahmed@gmail.com>
2026-05-10 00:17:00 +0600

    project skeleton

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("3D Car Driving Game");
+    glutMainLoop();
+    return 0;
+}
